5e85b9396ea89fb902f35ad4ba5943519f346a68
Jeffrey <jeferose4@gmail.com>
2024-04-29 17:35:45 -0600

    fixed modal

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 const movieListEl = document.querySelector(".movie__list");
 const searchValue = document.querySelector(".search__value");
-const n = 6;
+const n = 10;
 let movieData = {};
 let isModalOpen = false;
-
 
 // SEARCH MOVIES
 function searchTerm(event) {
@@ -13,90 +12,85 @@ function searchTerm(event) {
   searchValue.innerHTML = movieId;
 }
 
-
 // RENDER MOVIES
 function renderMovies() {
   if (movieData.Search) {
     movieListEl.innerHTML = movieData.Search
-    .map((movie) => getMovieDescription(movie))
-    .slice(0, n)
-    .join("");
+      .map((movie) => getMovieDescription(movie))
+      .slice(0, n)
+      .join("");
 
     document.querySelector('.no__movies--container').style.display = 'none';
     movieListEl.style.display = 'block';
-  }
-  else {
+  } else {
     console.log("Movie not found");
-      
+
     document.querySelector('.no__movies--container').style.display = 'block';
     movieListEl.style.display = 'none';
   }
 }
 
-
-// GETING MOVIES FROM BACKEND
+// GET MOVIES FROM BACKEND
 async function getMovies(movieId) {
-  const movies = await fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=fb20479&s=${movieId}`);
+  const movies = await fetch(`https://www.omdbapi.com/?apikey=fb20479&s=${movieId}`);
   movieData = await movies.json();
   setTimeout(() => {
     renderMovies();
-  }, 1000);
+  }, 2000);
 }
-
 
 // HTML GENERATION
 function getMovieDescription(movie) {
-  return `<div class="movie" onclick="toggleModal()">
+  return `<div class="movie" onclick="toggleModal('${movie.imdbID}')">
             <figure>
-                <img src="${movie.Poster}"alt="" class="movie__img"/>
+                <img src="${movie.Poster}" alt="" class="movie__img"/>
             </figure>
             <div class="movie__description--list">
                 <h3 class="movie__description movie__title">${movie.Title}</h3>
                 <p class="movie__description movie__year"><span class="movie__description--heading">Year:</span> ${movie.Year}</p>
             </div>
-          </div>
-
-          <div class="modal">
-            <div class="modal__half modal__about">
-              <figure>
-                <img src="${movie.Poster}"alt="" class="movie__img"/>
-              </figure>
-            </div>
-              
-            <div class="modal__half modal__contact">
-              <i class="fas fa-times modal__exit click" onclick="toggleModal()"></i>
-              <div>
-              <h3 class="movie__description movie__title">${movie.Title}</h3>
-              </div>
-            </div>
           </div>`;
 }
 
-function renderFilteredMovies(filteredMovies) {
-  movieListEl.innerHTML = filteredMovies
-    .slice(0, n)
-    .map((movie) => getMovieDescription(movie))
-    .join("");
-}
-
-
 // LOADING FUNCTION
-function loading(load) {
+function loading() {
   const loading = document.querySelector(".loading__spinner");
-
-  loading.classList += " loading__spinner--visible";
+  loading.classList.add("loading__spinner--visible");
   setTimeout(() => {
     loading.classList.remove("loading__spinner--visible");
   }, 2000);
 }
 
-
 // TOGGLE MODAL
-function toggleModal() {
+function toggleModal(imdbID) {
   if (isModalOpen) {
     isModalOpen = false;
-    return document.body.classList.remove("modal--open");
+    document.body.classList.remove("modal--open");
+  } else {
+    isModalOpen = true;
+    fetch(`https://www.omdbapi.com/?apikey=fb20479&i=${imdbID}`)
+      .then(response => response.json())
+      .then(data => renderMovieInfo(data));
+    document.body.classList.add("modal--open");
   }
-  isModalOpen = true;
-  document.body.classList += " modal--open";
+}
+
+// RENDER MOVIE INFO
+function renderMovieInfo(movieInfo) {
+  const modal = document.getElementById("modal");
+  modal.innerHTML = `
+    <div class="modal__half modal__about">
+      <figure>
+        <img src="${movieInfo.Poster}" alt="" class="modal__movie--img"/>
+      </figure>
+    </div>
+    <div class="modal__half modal__contact">
+      <i class="fas fa-times modal__exit click" onclick="toggleModal()"></i>
+      <div class="modal__movie--description">
+        <h3 class="movie__description modal__movie--title">${movieInfo.Title}</h3>
+        <h3 class="movie__description modal__movie--rated">${movieInfo.Rated}</h3>
+        <h3 class="movie__description modal__movie--plot">${movieInfo.Plot}</h3>
+        <h3 class="movie__description modal__movie--actors">${movieInfo.Actors}</h3>
+      </div>
+    </div>`;
 }
